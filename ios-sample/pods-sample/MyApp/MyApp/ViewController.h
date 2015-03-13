a9ca2d9f3fc26645d41d054bd7688716d7888368
Navneet Kumar <navneetk@bluejeans.com>
2015-03-13 16:29:17 -0700

    adding a pods sample

---
+++ ios-sample/pods-sample/MyApp/MyApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyApp
+//
+//  Created by Navneet Kumar on 13/03/15.
+//  Copyright (c) 2015 Bluejeans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ios-sample/pods-sample/MyApp/MyApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyApp
+//
+//  Created by Navneet Kumar on 13/03/15.
+//  Copyright (c) 2015 Bluejeans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
