3698039cfa5d7f40b5efc803d717c1add0f51fa0
Navneet Kumar <navneetk@bluejeans.com>
2015-03-13 16:58:19 -0700

    adding a pod

--- ios-sample/pods-sample/MyApp/MyApp/ViewController.m
+++ ios-sample/pods-sample/MyApp/MyApp/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <CocoaLumberjack/CocoaLumberjack.h>
 
 @interface ViewController ()
 
@@ -16,6 +17,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [DDLog addLogger:[DDASLLogger sharedInstance]];
+    [DDLog addLogger:[DDTTYLogger sharedInstance]];
+    
+    DDLogVerbose(@"hi");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
